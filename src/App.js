ad4e961774c486c4de206b1d073173262213139d
marimarafa <arafamarim166@gmail.com>
2024-11-15 17:09:46 +0100

    ..

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Example1 from './Esempi/Es_1';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Example1> </Example1>
     </div>
   );
 }
---
+++ src/Esempi/Es_1.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Example1 = () => {
+    return (
+        <div>
+            <h1> Esempio 1 di codice JSX</h1>
+            <p> Questo è un semplice esempio di codice JSX</p>
+        </div>
+    );
+};
+export default Example1;
