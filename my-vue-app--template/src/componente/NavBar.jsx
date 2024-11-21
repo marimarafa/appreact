932e0d4bd3d4562b2e4b524068b5b0c2533f0127
marimarafa <arafamarim166@gmail.com>
2024-11-21 12:53:31 +0100

    ..

---
+++ my-vue-app--template/src/componente/NavBar.jsx
@@ -0,0 +1,13 @@
+import './NavBar.css'
+
+function NavBar(){
+    return (
+        <ul>
+            <li><a href = "#"> Ciao </a></li>
+            <li><a href = "#"> Ciao </a></li>
+            <li><a href = "#"> Ciao </a></li>
+            <li><a href = "#"> Ciao </a></li>
+        </ul>
+    )
+}
+export default NavBar
