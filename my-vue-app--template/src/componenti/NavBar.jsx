1d7121e646d0557109455dec5a1858eab1b8dfb1
marimarafa <arafamarim166@gmail.com>
2024-12-02 09:42:37 +0100

    ..

--- my-vue-app--template/src/componenti/NavBar.jsx
+++ my-vue-app--template/src/componenti/NavBar.jsx
@@ -1,14 +1,31 @@
 import './NavBar.css'
-import Link from './Link'
+
+// creazione constanti
+const x = '-----><-----';
+const img1 = '/vite.svg'
+const img2 = '/src/assets/react.svg'
+const imgStyle = {
+height: '200px',
+borderRadius: '70px'
+}
 
 function NavBar(){
-    return (
-        <ul>
-            <li><Link></Link></li>
-            <li><Link></Link></li>
-            <li><Link></Link></li>
-            <li><Link></Link></li>
-        </ul>
+    return(
+    <>
+    <div id = 'box' className='box' ></div>
+    <nav> {x}</nav>
+    <img src = {img1} height={200}></img>
+    <img style={imgStyle} src = {img2}></img>
+    <p className='stilemio' > CORSO </p>
+    <p color='yellow' >________________</p>
+
+    <ul>
+        <li> <a href = "#" > ciao</a></li>
+        <li> <a href = "#" > ciao</a></li>
+        <li> <a href = "#" > ciao</a></li>
+        <li> <a href = "#" > ciao</a></li>       
+    </ul>
+    </>
     )
 }
 export default NavBar
